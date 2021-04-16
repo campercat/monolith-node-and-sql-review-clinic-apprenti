4aed73cd8aab76b9a1f6aeec626732899af4abb3
Suey Chen <suey.chen.16@gmail.com>
2021-04-15 22:08:21 -0400

    get and post using postgres and express

--- server/models/Cat.js
+++ server/models/Cat.js
@@ -5,6 +5,52 @@ const pool = new pg.Pool({
 })
 
 class Cat {
+  constructor({ id, name, human, age }) {
+    this.id = id
+    this.name = name
+    this.human = human
+    this.age = age
+  }
+
+  static async findAll() {
+    try {
+      const queryString = 'SELECT * FROM cats;'
+      const result = await pool.query(queryString)
+      const catData = result.rows
+      const cats = catData.map(cat => {
+        return new this(cat)
+      })
+      return cats
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static async findById(id) {
+    try {
+      const queryString = `SELECT * FROM cats WHERE id = ${id};`
+      const result = await pool.query(queryString)
+      const catData = result.rows[0]
+      return new this(catData)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  async save() {
+    try {
+      const queryString = 'INSERT INTO cats (name, human, age) VALUES ($1, $2, $3) RETURNING id;'
+      const result = await pool.query(queryString, [this.name, this.human, this.age])
+      const newCatId = result.rows[0].id
+      this.id = newCatId
+      return true
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
 
 }
 
--- server/routes/api/v1/catsRouter.js
+++ server/routes/api/v1/catsRouter.js
@@ -4,5 +4,37 @@ import Cat from "../../../models/Cat.js"
 
 const catsRouter = new express.Router()
 
+catsRouter.get("/", async (req, res) => {
+  try{
+    const cats = await Cat.findAll()
+    res.status(200).json({ cats: cats })
+  } catch(error) {
+    console.log(error)
+    res.status(500).json({ errors: error })
+  }
+})
+
+catsRouter.get("/:id", async (req, res) => {
+  try {
+    const catId = req.params.id
+    const cat = await Cat.findById(catId)
+    res.status(200).json({ cat: cat })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ errors: error })
+  }
+})
+
+catsRouter.post("/", async (req, res) => {
+  try {
+    const newCatData = req.body
+    const newCat = new Cat(newCatData)
+    await newCat.save()
+    res.status(201).json({ cat: newCat })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ errors: error })
+  }
+})
 
 export default catsRouter
